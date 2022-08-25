995fa47dd3dc18cc83b107d0982a35dfe147c2c9
Jeff <jbridges1119@gmail.com>
2022-08-25 16:33:39 -0600

    Add styling for question card

--- src/components/questionCard.tsx
+++ src/components/questionCard.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+//Types
+import {AnswerObject } from '../App'
+//Styles
+import {Wrapper, ButtonWrapper} from './QuestionCard.styles'
 
 type Props = {
   question: string;
   answers: string[];
-  callback: any;
-  userAnswer: any;
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
   questionNumber: number;
   totalQuestions: number;
 };
@@ -17,20 +21,25 @@ const QuestionCard: React.FC<Props> = ({
   questionNumber,
   totalQuestions,
 }) => {
-  return (<div>
+  return (
+  <Wrapper>
     <p className="nubmer">
       Question: {questionNumber} / {totalQuestions}
     </p>
     <p dangerouslySetInnerHTML={{ __html: question}}/>
     <div>
       {answers.map(answer => (
-        <div key={answer}>
-          <button disabled={userAnswer} value={answers} onClick={callback}>
+        <ButtonWrapper key={answer}
+        correct={userAnswer?.correctAnswer === answer}
+        userClicked={userAnswer?.answer === answer}
+        >
+          <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
             <span dangerouslySetInnerHTML={{ __html: answer}}/>
           </button>
-        </div>
+        </ButtonWrapper>
       ))}
     </div>
-  </div>);
+    </Wrapper>
+  )
 };
 export default QuestionCard;
