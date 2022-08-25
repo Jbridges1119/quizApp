409666e3226d2103c1e60d971b78efdceb02f812
Jeff <jbridges1119@gmail.com>
2022-08-25 12:59:23 -0600

    Add logic and layout

--- src/components/questionCard.tsx
+++ src/components/questionCard.tsx
@@ -1,6 +1,36 @@
 import React from "react";
 
-const QuestionCard = () => {
-  return <div>Qustion Card</div>;
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => {
+  return (<div>
+    <p className="nubmer">
+      Question: {questionNumber} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question}}/>
+    <div>
+      {answers.map(answer => (
+        <div>
+          <button disabled={userAnswer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer}}/>
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>);
 };
 export default QuestionCard;
