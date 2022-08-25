41edaa7579ebab5ec24c21eefadde65590ca54f5
Jeff <jbridges1119@gmail.com>
2022-08-25 13:07:18 -0600

    Add prop routes for QuestionCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 //Components
-import QuestionCard from './components/questionCard'
+import QuestionCard from "./components/questionCard";
 
+const TOTAL_QUESTIONS = 10;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswer, setUserAnswers] = useState([]);
+  const [score, setScrore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
   const startQuiz = async () => {};
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
@@ -17,8 +25,17 @@ function App() {
       </button>
       <p className="score">Score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard/>
-      <button className="next" onClick={nextQuestion}>Next Question</button>
+      <QuestionCard
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswer ? userAnswer[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
